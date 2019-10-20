07dbe121f0e7e9722a5f0df59c3924a68b1d30db
Nick Garvey <garveyness03@gmail.com>
2019-10-19 21:36:47 -0400

    Small adjustments to settings page

--- app/src/main/java/com/nick/conwaygameoflife/SettingsFragment.kt
+++ app/src/main/java/com/nick/conwaygameoflife/SettingsFragment.kt
@@ -1,11 +1,22 @@
 package com.nick.conwaygameoflife
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val lifespanPreference: EditTextPreference?
+                = findPreference(getString(R.string.lifespan_key))
+
+        lifespanPreference?.setOnBindEditTextListener {
+            it.inputType = InputType.TYPE_CLASS_NUMBER
+        }
     }
+
+
 }
