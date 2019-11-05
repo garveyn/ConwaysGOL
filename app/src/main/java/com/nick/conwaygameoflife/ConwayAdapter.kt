50a6955dd806afea35e4fb6c19535392823fc640
garveyn <garveyness03@gmail.com>
2019-11-05 16:07:22 -0500

    Started Game logic Implementation

--- app/src/main/java/com/nick/conwaygameoflife/ConwayAdapter.kt
+++ app/src/main/java/com/nick/conwaygameoflife/ConwayAdapter.kt
@@ -1,11 +1,12 @@
 package com.nick.conwaygameoflife
 
+import android.animation.ArgbEvaluator
 import android.content.Context
-import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,8 +20,35 @@ class ConwayAdapter(var cellArr: ArrayList<Cell>, val context: Context)
         val cellImage: ImageView = itemView.findViewById(R.id.cell_imageView)
         private lateinit var cell: Cell
 
-        fun bind(cell: Cell){
-            this.cell = cell
+        fun bind(cellToBind: Cell){
+            this.cell = cellToBind
+
+            val lifespan = preferences.getInt(context.getString(R.string.lifespan_key),
+                ConwayFragment.IMMORTAL)
+
+            cellImage.setColorFilter(getCellColor(lifespan))
+
+            // TODO Setup cell Animation
+        }
+
+        private fun getCellColor(lifespan: Int) : Int {
+            val backgroundColor = preferences.getInt(context.getString(R.string.cc_dead_key),
+                ContextCompat.getColor(context, R.color.default_dark))
+            val livingColor = preferences.getInt(context.getString(R.string.cc_live_key),
+                ContextCompat.getColor(context, R.color.default_light))
+            val oldColor = preferences.getInt(context.getString(R.string.cc_old_key),
+                ContextCompat.getColor(context, R.color.default_light_faded))
+
+            return when {
+                !cell.isLiving -> backgroundColor
+
+                cell.age == 0 || lifespan == ConwayFragment.IMMORTAL -> livingColor
+
+                else -> {
+                    val relAge = cell.age.toFloat()/lifespan
+                    ArgbEvaluator().evaluate(relAge, livingColor, oldColor) as Int
+                }
+            }
         }
 
     }
--- app/src/main/java/com/nick/conwaygameoflife/ConwayFragment.kt
+++ app/src/main/java/com/nick/conwaygameoflife/ConwayFragment.kt
@@ -1,35 +1,43 @@
 package com.nick.conwaygameoflife
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
 import android.widget.Button
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ConwayFragment : Fragment() {
-
-    // Colors (int Hex Strings)
-    var livingColor:        Int = 0
-    var deadColor:          Int = 0
-    var backgroundColor:    Int = 0
 
     // Game state
     var isPlaying:  Boolean = false
     var playSpeed:  Int = 1 // May not be used
     var gameBoard:  ArrayList<Cell> = ArrayList(20*20)
+    var changes: ArrayList<Int> = ArrayList()
 
-    // Buttons
-    lateinit var playPauseButton:   Button
-    lateinit var saveButton:        Button
-    lateinit var loadButton:        Button
-    lateinit var cloneButton:       Button
+    //Preferences
+    private lateinit var sharedPreferences: SharedPreferences
+
+    // Screen elements
+    private lateinit var playPauseButton:   Button
+    private lateinit var saveButton:        Button
+    private lateinit var loadButton:        Button
+    private lateinit var cloneButton:       Button
+    private lateinit var recyclerView:      RecyclerView
 
     companion object {
 
         // Bundle & intent keys
         const val PLAY_KEY = "play"
         const val BOARD_KEY = "board"
+
+        // Functional Constants
+        const val IMMORTAL = 0
+        const val SURVIVAL_MIN = 2
+        const val SURVIVAL_MAX = 3
+        const val BIRTH_NUMBER = 3
 
     }
 
@@ -42,6 +50,7 @@ class ConwayFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.gol_view, container, false)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
         // Store buttons and setup actions
         // Trying not to use synthetics anymore...
@@ -50,20 +59,27 @@ class ConwayFragment : Fragment() {
         loadButton = view.findViewById(R.id.load_button)
         cloneButton = view.findViewById(R.id.clone_button)
 
+        playPauseButton.setOnClickListener {
+            updateBoard()
+        }
+
+        // Setup RecyclerView
+        val boardSize = sharedPreferences.getString(
+            getString(R.string.gridsize_key), "20")!!.toInt()
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.game_screen).apply {
+            adapter = ConwayAdapter(gameBoard, context)
+            layoutManager = GridLayoutManager(context, boardSize)
+        }
+
 
         return view
     }
 
     override fun onResume() {
         super.onResume()
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         playSpeed = sharedPreferences.getInt(getString(R.string.game_speed_key), 1)
-        backgroundColor = sharedPreferences.getInt(getString(R.string.cc_dead_key),
-            ContextCompat.getColor(requireContext(), R.color.default_dark))
-        livingColor = sharedPreferences.getInt(getString(R.string.cc_live_key),
-            ContextCompat.getColor(requireContext(), R.color.default_light))
-        deadColor = sharedPreferences.getInt(getString(R.string.cc_old_key),
-            ContextCompat.getColor(requireContext(), R.color.default_light_faded))
+        updateUI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -80,6 +96,11 @@ class ConwayFragment : Fragment() {
                 }
                 true
             }
+            R.id.action_reset -> {
+                resetBoard(sharedPreferences.getString(
+                    getString(R.string.gridsize_key), "20")!!.toInt())
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -93,7 +114,115 @@ class ConwayFragment : Fragment() {
     }
 
     fun updateUI() {
+        val newBoardSize = sharedPreferences.getString(
+            getString(R.string.gridsize_key), "20")!!.toInt()
+
+        if (gameBoard.size != newBoardSize*newBoardSize) {
+            // Reset board with new size
+            resetBoard(newBoardSize)
+        }
+    }
+
+    private fun updateBoard() {
+        gameBoard = calculateNewBoardState()
+        for (change in changes) {
+            recyclerView.adapter?.notifyItemChanged(change)
+        }
+    }
+
+    private fun resetBoard(newBoardSize: Int) {
+        gameBoard = ArrayList(newBoardSize*newBoardSize)
+        val adapter = recyclerView.adapter as ConwayAdapter
+        adapter.cellArr = gameBoard
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun calculateNewBoardState() : ArrayList<Cell> {
+        val lifeExpencancy = sharedPreferences.getInt(getString(R.string.lifespan_key), 0)
+        val livingCells = getLivingCells()
+        val newBoard = gameBoard
+        val deadCells = mutableSetOf<Int>()
+
+        // Calculate deaths
+        for (index in livingCells) {
+            val cell = gameBoard[index]
+            var livingNeighbors = 0
+
+            // Handle Age
+            if (lifeExpencancy != 0) {
+                cell.age++
+                changes.add(index) // Aging always changes View
+                if (cell.age >= lifeExpencancy){
+                    // Dies of old age
+                    newBoard[index] = Cell()
+                    continue
+                }
+                newBoard[index].age = cell.age
+            }
+
+            // Handle Neighbors
+            for (neighbor in getNeighbors(index)) {
+                if (gameBoard[neighbor].isLiving) {
+                    livingNeighbors++
+                } else if (!deadCells.contains(neighbor)) {
+                    deadCells.add(neighbor)
+                }
+            }
+
+            // Dies if over or under populated
+            if (livingNeighbors < SURVIVAL_MIN || livingNeighbors > SURVIVAL_MAX) {
+                newBoard[index] = Cell()
+                changes.add(index)
+            }
+        }
+
+        // Calculate births
+        for (index in deadCells) {
+            val cell = gameBoard[index]
+            var livingNeighbors = 0
+
+            for (neighbor in getNeighbors(index)) {
+                if (gameBoard[neighbor].isLiving) {
+                    livingNeighbors++
+                }
+            }
+
+            if (livingNeighbors == BIRTH_NUMBER) {
+                cell.isLiving = true
+                newBoard[index] = cell
+                changes.add(index)
+            }
+        }
+
+        // Return new board state
+        return newBoard
+    }
+
+    private fun getLivingCells() : List<Int> {
+        return gameBoard.withIndex()
+            .filter {( _, cell) -> cell.isLiving }
+            .map { (index, _ ) -> index}
+    }
+
+    private fun getNeighbors(position: Int) : List<Int> {
+        val boardSize = sharedPreferences.getString(
+            getString(R.string.gridsize_key), "20")!!.toInt()
+
+        // Due to gridManager, the 1D array needs to be translated to a 2D array
+        val (x, y) = transTo2D(position, boardSize)
+        val neighbor2D = listOf(Pair(x+1, y), Pair(x+1, y+1), Pair(x, y+1),
+            Pair(x-1, y+1), Pair(x-1, y), Pair(x-1, y-1),
+            Pair(x, y-1), Pair(x+1, y-1))
+
+        // Normalize on edges, and translate back to 1D
+        return neighbor2D
+            .map { (x, y) -> Pair((x + boardSize) % boardSize, (y + boardSize) % boardSize) }
+            .map { (x, y) -> transTo1D(x, y, boardSize) }
 
     }
+
+    private fun transTo2D(index: Int, boardSize: Int) = Pair(index % boardSize, index / boardSize)
+
+    private fun transTo1D(x: Int, y: Int, boardSize: Int) = (y * boardSize) + x
 
 }
